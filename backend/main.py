361c2c2efc8eb4a2c177ffe0b5ae344b209a995b
Kibet A. Maswan <user@users-MacBook-Pro.local>
2026-03-01 12:16:54 -0500

    better

--- backend/main.py
+++ backend/main.py
@@ -18,9 +18,8 @@ def startup():
 @app.post("/predict")
 async def get_prediction(data:BearingInput):
     result = predict(data.vib_x, data.vib_y, data.vib_z)
-    return {'predicted_wear': result}
+    return {'predicted_wear': f'{result:.2f}'}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
     
